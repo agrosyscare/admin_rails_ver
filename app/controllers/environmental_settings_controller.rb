c7ec1ec9fb531bf46a8c31f96725ea5f2ee91f9b
frodrigueztorres <felipe.andres.rodt@gmail.com>
2021-03-28 23:13:53 -0300

    Adding view to environmental setting

--- app/controllers/environmental_settings_controller.rb
+++ app/controllers/environmental_settings_controller.rb
@@ -1,5 +1,12 @@
 class EnvironmentalSettingsController < ApplicationController
   def index
     @environmental_conditions = EnvironmentalCondition.all
+    @environmental_settings = EnvironmentalSetting.new
+  end
+
+  private
+
+  def environmental_setting_params
+    params.require(:environmental_setting).permit(:min_value, :max_value, :floor_id, :environmental_condition_id)
   end
 end
