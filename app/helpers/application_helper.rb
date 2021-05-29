0a3651a29c5a9cf5e746d144eb4c5dd662766261
Felipe Rodriguez Torres <felipe@rayoslab.com>
2021-05-29 02:39:37 -0400

    adding icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,38 +7,46 @@ module ApplicationHelper
   #   end
   # end
 
-  # def link_to_new(url, options={})
-  #   title = options[:title] || t(:new, scope: [:views, :buttons])
-  #   icon = options[:icon] || 'fas fa-plus'
-  #   link_to url, data: {toggle: "tooltip", animation: false}, title: title do
-  #     content_tag(:i, '', class: "#{icon} px-1")
-  #   end
-  # end
+  def link_to_chart(url, options={})
+    title = options[:title] || t(:list, scope: [:views, :buttons])
+    icon = options[:icon] || 'auto_graph'
+    link_to url, data: {toggle: "tooltip", animation: false}, title: title do
+      content_tag(:i, icon, class: "material-icons", style: "color:purple")
+    end
+  end
 
-  # def link_to_show(url, options={})
-  #   title = options[:title] || t(:show, scope: [:views, :buttons])
-  #   icon = options[:icon] || 'fas fa-eye'
-  #   link_to url, data: {toggle: "tooltip", animation: false}, title: title do
-  #     content_tag(:i, '', class: "#{icon} px-1")
-  #   end
-  # end
+  def link_to_new(url, options={})
+    title = options[:title] || t(:new, scope: [:views, :buttons])
+    icon = options[:icon] || 'add'
+    link_to url, data: {toggle: "tooltip", animation: false}, title: title do
+      content_tag(:i, icon, class: "material-icons")
+    end
+  end
 
-  # def link_to_edit(url, options={})
-  #   title = options[:title] || t(:edit, scope: [:views, :buttons])
-  #   icon = options[:icon] || 'fas fa-pen'
-  #   link_to url, data: { toggle: "tooltip", animation: false }, title: title do
-  #     content_tag(:i, '', class: "#{icon} px-1")
-  #   end
-  # end
+  def link_to_show(url, options={})
+    title = options[:title] || t(:show, scope: [:views, :buttons])
+    icon = options[:icon] || 'person'
+    link_to url, data: {toggle: "tooltip", animation: false}, title: title do
+      content_tag(:i, icon, class: "material-icons", style: "color:#00bcd4")
+    end
+  end
 
-  # def link_to_destroy(url, options={})
-  #   title = options[:title] || t(:destroy, scope: [:views, :buttons])
-  #   confirm = options[:confirm] || t(:destroy, scope: [:views, :confirmations])
-  #   icon = options[:icon] || 'fas fa-trash'
-  #   link_to url, data: { confirm: confirm, toggle: "tooltip", animation: false }, method: :delete, title: title do
-  #     content_tag(:i, '', class: "#{icon} px-1")
-  #   end
-  # end
+  def link_to_edit(url, options={})
+    title = options[:title] || t(:edit, scope: [:views, :buttons])
+    icon = options[:icon] || 'edit'
+    link_to url, data: { toggle: "tooltip", animation: false }, title: title do
+      content_tag(:i, icon, class: "material-icons")
+    end
+  end
+
+  def link_to_destroy(url, options={})
+    title = options[:title] || t(:destroy, scope: [:views, :buttons])
+    confirm = options[:confirm] || t(:destroy, scope: [:views, :confirmations])
+    icon = options[:icon] || 'close'
+    link_to url, data: { confirm: confirm, toggle: "tooltip", animation: false }, method: :delete, title: title do
+      content_tag(:i, icon, class: "material-icons", style: "color:red")
+    end
+  end
 
   # def link_to_reload(url, options={})
   #   title = options[:title] || t(:reload, scope: [:views, :buttons])
