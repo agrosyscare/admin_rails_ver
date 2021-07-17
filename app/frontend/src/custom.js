c0370241b53bd919b8a766cc289415a098bd5066
Felipe Rodriguez Torres <felipe@rayoslab.com>
2021-07-16 21:10:10 -0400

    improvements

--- app/frontend/src/custom.js
+++ app/frontend/src/custom.js
@@ -1,7 +1,6 @@
-document.addEventListener("turbolinks:load", function() {
-  $(".nav a").on("click", function() {
-    debugger
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-});
+// document.addEventListener("turbolinks:load", function() {
+//   $(".nav a").on("click", function() {
+//     $(".nav").find(".active").removeClass("active");
+//     $(this).parent().addClass("active");
+//   });
+// });
