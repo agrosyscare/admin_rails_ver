33897cd7f45637e121d5767c690d7df5ea7d9cf6
Felipe Rodriguez Torres <felipe@rayoslab.com>
2021-07-31 14:23:48 -0400

    configuring deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "18.219.254.32", user: "deploy", roles: %w{app db web}
+server "18.117.184.17", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -41,10 +41,11 @@ server "18.219.254.32", user: "deploy", roles: %w{app db web}
 #
 # Global options
 # --------------
- set :ssh_options, {
-   keys: %w(~/.ssh/aws-connection.pem),
-   forward_agent: true
- }
+#  set :ssh_options, {
+#    keys: %w(/home/user_name/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
