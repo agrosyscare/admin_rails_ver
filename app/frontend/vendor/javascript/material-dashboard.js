a6f928b2053baf270934aa907a4b881fb76e380e
Felipe Rodriguez Torres <felipe@rayoslab.com>
2021-08-02 22:15:51 -0400

    disable perfect scrollbar

--- app/frontend/vendor/javascript/material-dashboard.js
+++ app/frontend/vendor/javascript/material-dashboard.js
@@ -16,17 +16,17 @@
  */
 
 (function() {
-  isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+  // isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-//   if (isWindows) {
-//     // if we are on windows OS we activate the perfectScrollbar function
-//     $('.sidebar .sidebar-wrapper, .main-panel, .main').perfectScrollbar();
+  // if (isWindows) {
+  //   // if we are on windows OS we activate the perfectScrollbar function
+  //   $('.sidebar .sidebar-wrapper, .main-panel, .main').perfectScrollbar();
 
-//     $('html').addClass('perfect-scrollbar-on');
-//   } else {
-//     $('html').addClass('perfect-scrollbar-off');
-//   }
-// })();
+  //   $('html').addClass('perfect-scrollbar-on');
+  // } else {
+  //   $('html').addClass('perfect-scrollbar-off');
+  // }
+})();
 
 
 var breakCards = true;
